0bdd8bf5d682862ddebe90df590531db420fb54a
Janek <janek35515@gmail.com>
2025-01-26 18:32:37 +0100

    Add function to play one round
    Added function playRound that let user play one round

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    const options = ['rock', 'paper', 'scissors'];
-    const computerChoice = Math.floor(Math.random() * options.length);
+  const options = ["rock", "paper", "scissors"];
+  const computerChoice = Math.floor(Math.random() * options.length);
 
-    return options[computerChoice];
+  return options[computerChoice];
 }
-
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-    const humanChoice = prompt('Rock, paper or scissors?')
-    return humanChoice
+  const humanChoice = prompt("Rock, paper or scissors?");
+  return humanChoice;
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+  const humanChoiceCase = humanChoice.toLowerCase();
+
+  if (humanChoiceCase !== computerChoice) {
+    if (
+      (humanChoiceCase === "rock" && computerChoice === "scissors") ||
+      (humanChoiceCase === "paper" && computerChoice === "rock") ||
+      (humanChoiceCase === "scissors" && computerChoice === "paper")
+    ) {
+      console.log(`You win! ${humanChoiceCase} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoiceCase}`);
+      computerScore++;
+    }
+  } else {
+    console.log("Tie!");
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
